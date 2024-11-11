838d3dd6ed79e9502627fe747ecd86403312ce23
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-11 11:04:09 -0500

    Finalized files

---
+++ Functions-Lists-Classes-HBM.py
@@ -0,0 +1,85 @@
+# Programming Assingment: Case Study-Functions, Lists, and Classes
+# Progammer: Hershey Marbeda
+# Last Date Updated: 11.11.2024
+
+# Programming Assignment: Case Study-Functions, Lists, and Classes
+# Write a Python app that has the following classes:
+# A super class called Vehicle, which contains an attribute for vehicle type, 
+# such as car, truck, plane, boat, or a broomstick.
+# A class called Automobile which will inherit the attributes from Vehicle and 
+# also contain the following attributes:
+# year
+# make
+# model
+# doors (2 or 4)
+# roof (solid or sun roof).
+
+# Write an app that will accept user input for a car. The app will store "car"
+# into the vehicle type in your Vehicle super class. 
+# The app will then ask the user for the year, make, model, doors, and type of 
+# roof and store thdata in the attributes above.
+# The app will then output the data in an easy-to-read and understandable format, 
+# such as this:
+#   Vehicle type: car
+#   Year: 2022
+#   Make: Toyota
+#   Model: Corolla
+#   Number of doors: 4
+#   Type of roof: sun roof
+
+# Super Class of Vehicle
+class Vehicle:
+    def __init__(self, vehicle_type, year, make, model, doors, roof):
+        self.vehicle_type = vehicle_type
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+        
+    # Function to display the vehicle information
+    def display_inputs(self):
+        print("\nYour Vehicle Information:")
+        print(f"Vehicle type: {self.vehicle_type}")
+        print(f"Year: {self.year}")
+        print(f"Make: {self.make}")
+        print(f"Model: {self.model}")
+        print(f"Number of doors: {self.doors}")
+        print(f"Type of roof: {self.roof} \n")
+        
+# Sub Class of Vehicle    
+class Automobile(Vehicle):
+    def __init__(self, vehicle_type, year, make, model, doors, roof):
+        # inherit the attributes from Vehicle
+        super().__init__(vehicle_type, year, make, model, doors, roof)
+        
+print("\nEnter the following information for your Vehicle:")
+allowed_vehicle_types = ["Car", "Truck", "Plane", "Boat", "Broomstick"]
+vehicle_type = input("Vehicle Type (Car, Truck, Plane, Boat, Broomstick): ").capitalize()
+while vehicle_type not in allowed_vehicle_types:
+    print("\nInvalid vehicle type. Please enter car, truck, plane, boat, or broomstick.")
+    vehicle_type = input("Vehicle Type (car, truck, plane, boat, broomstick): ").capitalize()
+
+year = input("Year: ")
+if year.isdigit() == False:
+    print("\nInvalid year. Please enter a number.")
+    year = input("Year: ")
+     
+make = input("Make: ").capitalize()
+model = input("Model: ").capitalize()
+
+doors = input("Number of doors (2 or 4): ")
+while doors != "2" and doors != "4":
+    print("\nInvalid number of doors. Please enter 2 or 4.")
+    doors = input("Number of doors (2 or 4): ")
+    
+roof = input("Type of roof (solid or sun roof): ").capitalize()
+while roof != "Solid" and roof != "Sun roof":
+    print("\nInvalid type of roof. Please enter Solid or Sun roof.")
+    roof = input("Type of roof (solid or sun roof): ").capitalize()
+
+        
+# To call functions        
+vehicle = Automobile(vehicle_type, year, make, model, doors, roof)
+vehicle.display_inputs()
+
